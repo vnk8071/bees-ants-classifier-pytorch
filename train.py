9347a40188fc5fb4df443686cf14adb857f15849
VNK <nguyenkhoi8071@gmail.com>
2021-08-18 04:17:00 +0700

    Add main file train.py

---
+++ train.py
@@ -0,0 +1,16 @@
+from src.trainer import BeeAntClassifier
+import json
+
+CONFIG_PATH = './config/train_config.json'
+params = json.load(open(CONFIG_PATH, 'r'))
+model = BeeAntClassifier(**params)
+
+
+def main():
+    model.train_model()
+
+
+if __name__ == '__main__':
+    print('Start training')
+    main()
+    print('Done')
